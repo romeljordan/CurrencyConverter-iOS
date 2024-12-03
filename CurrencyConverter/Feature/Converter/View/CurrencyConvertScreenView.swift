90f7fe20518dc1d4bb36f1d5d8b57cabc4dd61f2
romeljordan <romeljordan25@gmail.com>
2024-12-03 14:59:32 +0800

    Fix sheet not showing after dragging down

--- CurrencyConverter/Feature/Converter/View/CurrencyConvertScreenView.swift
+++ CurrencyConverter/Feature/Converter/View/CurrencyConvertScreenView.swift
@@ -81,52 +81,58 @@ struct CurrencyConvertScreenView: View {
         }
         .padding()
         .background(Color.black)
-        .sheet(isPresented: $isCountryListPopupShown, content: {
-            let selectedItems = switch countryListInputType {
-            case .base: [viewModel.screenState.baseCountry]
-            case .converted(let current): [current]
-            case .new: viewModel.screenState.selectedCountryList.map({ $0.code })
-            case .none: [String]()
-            }
-            
-            let disabledItems = switch countryListInputType {
-            case .base: viewModel.screenState.selectedCountryList.map{ $0.code }
-            case .converted(let current): viewModel.screenState.selectedCountryList
-                    .filter { $0.code != current }
-                    .map{ $0.code } + [viewModel.screenState.baseCountry]
-            case .new: [viewModel.screenState.baseCountry]
-            case .none: [String]()
-            }
-            
-            SelectionCountryListScreenView(
-                list: viewModel.screenState.countryList,
-                initialSelected: selectedItems,
-                isMultiple: (countryListInputType == .new),
-                disabledItems: disabledItems,
-                onClose: {
-                    countryListInputType = nil
-                },
-                onResults: { result in
-                    switch countryListInputType {
-                    case .base:
-                        guard let code = result.first else { break }
-                        viewModel.updateBaseCountry(code: code)
-                    case .converted(let current):
-                        guard let newCode = result.first else { break }
-                        viewModel.updateSelectedCountryToConvert(old: current, new: newCode)
-                    case .new:
-                        viewModel.addCountryToSelection(countryList: result)
-                    case .none: countryListInputType = nil
-                    }
-
-                    countryListInputType = nil
+        .sheet(
+            isPresented: $isCountryListPopupShown,
+            onDismiss: {
+                countryListInputType = nil
+            },
+            content: {
+                let selectedItems = switch countryListInputType {
+                case .base: [viewModel.screenState.baseCountry]
+                case .converted(let current): [current]
+                case .new: viewModel.screenState.selectedCountryList.map({ $0.code })
+                case .none: [String]()
                 }
-            )
-                .presentationDetents([.fraction(1), .fraction(1)])
-                .presentationDragIndicator(.hidden)
-        })
-        .onChange(of: countryListInputType) { value in
-            isCountryListPopupShown = (countryListInputType != nil)
+                
+                let disabledItems = switch countryListInputType {
+                case .base: viewModel.screenState.selectedCountryList.map{ $0.code }
+                case .converted(let current): viewModel.screenState.selectedCountryList
+                        .filter { $0.code != current }
+                        .map{ $0.code } + [viewModel.screenState.baseCountry]
+                case .new: [viewModel.screenState.baseCountry]
+                case .none: [String]()
+                }
+                
+                SelectionCountryListScreenView(
+                    list: viewModel.screenState.countryList,
+                    initialSelected: selectedItems,
+                    isMultiple: (countryListInputType == .new),
+                    disabledItems: disabledItems,
+                    onClose: {
+                        countryListInputType = nil
+                    },
+                    onResults: { result in
+                        switch countryListInputType {
+                        case .base:
+                            guard let code = result.first else { break }
+                            viewModel.updateBaseCountry(code: code)
+                        case .converted(let current):
+                            guard let newCode = result.first else { break }
+                            viewModel.updateSelectedCountryToConvert(old: current, new: newCode)
+                        case .new:
+                            viewModel.addCountryToSelection(countryList: result)
+                        case .none: countryListInputType = nil
+                        }
+
+                        countryListInputType = nil
+                    }
+                )
+                    .presentationDetents([.fraction(1), .fraction(1)])
+                    .presentationDragIndicator(.hidden)
+            }
+        )
+        .onChange(of: countryListInputType, initial: false) {
+            isCountryListPopupShown = countryListInputType != nil
         }
     }
 }
